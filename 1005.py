a6436e98e6814240b84b93b9055a24ee3a6dbf83
Jorge Clênio <cleniocontato@gmail.com>
2021-09-20 20:00:41 -0300

    Create 1005.py

---
+++ 1005.py
@@ -0,0 +1,8 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1005
+# -*- coding: utf-8 -*-
+a = float(input())
+b = float(input())
+
+media = ((a*3.5)+(b*7.5))/11
+
+print("MEDIA = {:.5f}".format(media))
