0aaeb7cd210c84b350b4e31d8ac01f21bf352598
Jorge Clênio <cleniocontato@gmail.com>
2021-09-21 01:59:25 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,10 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1008
+# -*- coding: utf-8 -*-
+numero = int(input())
+qtddHoras = int(input())
+valorHora = float(input())
+
+salario = qtddHoras * valorHora
+
+print("NUMBER = {}".format(numero))
+print("SALARY = U$ {:.2f}".format(salario))
