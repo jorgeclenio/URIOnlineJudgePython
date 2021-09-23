1653653bb82a7434f8b3b948716f8986820799b8
Jorge Clênio <cleniocontato@gmail.com>
2021-09-23 00:12:06 -0300

    Create 1011.py

---
+++ 1011.py
@@ -0,0 +1,8 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1011
+# -*- coding: utf-8 -*-
+r = float(input())
+
+pi = 3.14159
+volume = (4.0/3.0) * pi * r * r * r
+
+print("VOLUME = {:.3f}".format(volume))
