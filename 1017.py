1825b6efb16d2e263f2ea646880176659ff07291
Jorge Clênio <cleniocontato@gmail.com>
2021-09-27 10:08:45 -0300

    Create 1017.py

---
+++ 1017.py
@@ -0,0 +1,6 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1017
+# -*- coding: utf-8 -*-
+tGasto = int(input())
+vMedia = int(input())
+
+print("{:.3f}".format((tGasto * vMedia) / 12))
