dc89bb2f6a0434d170ed40cf0e87f9fc36449447
Jorge Clênio <cleniocontato@gmail.com>
2021-10-12 00:03:04 -0300

    Create 1168.py

---
+++ strings/1168.py
@@ -0,0 +1,27 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1168
+# -*- coding: utf-8 -*-
+for j in range(1, int(input())+1):
+    led = 0
+    x = input()
+    for i in range(0, len(x)):
+        if x[i] == '1':
+            led = led + 2
+        if x[i] == '2':
+            led = led + 5
+        if x[i] == '3':
+            led = led + 5
+        if x[i] == '4':
+            led = led + 4
+        if x[i] == '5':
+            led = led + 5
+        if x[i] == '6':
+            led = led + 6
+        if x[i] == '7':
+            led = led + 3
+        if x[i] == '8':
+            led = led + 7
+        if x[i] == '9':
+            led = led + 6
+        if x[i] == '0':
+            led = led + 6
+    print('{} leds'.format(led))
