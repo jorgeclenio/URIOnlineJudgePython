7e0b181b5fe65827ad94f0b5adfe4f5527a223f5
Jorge Clênio <cleniocontato@gmail.com>
2021-10-16 02:11:59 -0300

    Create 1036.py

---
+++ iniciante/1036.py
@@ -0,0 +1,11 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1036
+# -*- coding: utf-8 -*-
+a, b, c = map(float, input().split())
+
+if a == 0.0 or (b ** 2 - 4 * a * c) < 0:
+    print('Impossivel calcular')
+else:
+    x1 = (- b + (b ** 2 - 4 * a * c) ** (1/2))/(2 * a)
+    x2 = (- b - (b ** 2 - 4 * a * c) ** (1/2))/(2 * a)
+    print('R1 = {:.5f}'.format(x1))
+    print('R2 = {:.5f}'.format(x2))
