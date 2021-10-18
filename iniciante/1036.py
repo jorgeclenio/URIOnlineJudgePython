468d2e865a77cb53f9f1eed1a4c4f93886e0c366
Jorge Clênio <cleniocontato@gmail.com>
2021-10-18 18:28:01 -0300

    Update 1036.py

--- iniciante/1036.py
+++ iniciante/1036.py
@@ -7,5 +7,5 @@ if a == 0.0 or (b ** 2 - 4 * a * c) < 0:
 else:
     x1 = (- b + (b ** 2 - 4 * a * c) ** (1/2))/(2 * a)
     x2 = (- b - (b ** 2 - 4 * a * c) ** (1/2))/(2 * a)
-    print('R1 = {:.5f}'.format(x1))
-    print('R2 = {:.5f}'.format(x2))
+    print(f'R1 = {x1:.5f}')
+    print(f'R2 = {x2:.5f}')
